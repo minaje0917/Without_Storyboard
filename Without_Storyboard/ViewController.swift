9de1a82c0e6404b2841e8be2d4557ded83f664f8
minaje0917 <k050918@naver.com>
2022-05-17 12:31:47 +0900

    Initial Commit

---
+++ Without_Storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Without_Storyboard
+//
+//  Created by 선민재 on 2022/05/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
